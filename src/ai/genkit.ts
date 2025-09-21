297caf2c53923333bfcef7967b64cf252911dc1c
Updesh Karanwal <updeshkaranwal151@gmail.com>
2025-09-21 11:02:34 +0000

    So frustrated Error: Unable to determine type of tool: undefined     at

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,5 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI, googleSearch} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
