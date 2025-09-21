b75590d3f30b92ac20d8bce892bffc9f658146ff
Updesh Karanwal <updeshkaranwal151@gmail.com>
2025-09-21 08:13:25 +0000

    I know now for this action you want my api key of firebase here it is co

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,6 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PULIC_FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
