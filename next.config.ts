4bda29475cdca0ffa1c6f0db037d0857afd1a7db
Updesh Karanwal <updeshkaranwal151@gmail.com>
2025-10-07 03:18:41 +0000

    Bro app publish nahi ho pa raha 08:34:17.451 Running build in Washington

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -29,6 +30,12 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+   webpack: (config) => {
+    config.externals.push({
+      canvas: "commonjs canvas",
+    });
+    return config;
   },
 };
 
